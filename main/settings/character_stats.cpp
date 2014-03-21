c824d1387933635f165394bb8a192af95e59c2de
Blake Milner <blake@blake-VPCEG16FM.(none)>
2014-03-20 20:00:41 -0400

    Tidied up character stats.

--- main/settings/character_stats.cpp
+++ main/settings/character_stats.cpp
@@ -1,6 +1,7 @@
 #include "character_stats.hpp"
 
-// starting statistics for all characters
+// starting statistics for all characters, set more class-
+// specific stats in respective constructors
 Stats::Stats(){
-	VIS_RAD = 6;
+	VIS_RAD = Char_stats::DEFAULT_VIS_RAD;
 }
--- main/settings/character_stats.hpp
+++ main/settings/character_stats.hpp
@@ -3,6 +3,10 @@
 
 #define MAX_VIS_RAD 20
 
+namespace Char_stats{
+	static int DEFAULT_VIS_RAD = 6;
+};
+
 class Stats{
 public:
 	int VIS_RAD;
