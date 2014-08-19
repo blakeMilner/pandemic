979ee35fe0ea2aadeaf56e3e49cab250e5a9f52d
Blake Milner <blake@blake-VPCEG16FM.(none)>
2014-08-18 21:18:55 -0500

    Added space for large scale viewer and added outline for class to take mouseEvents.

--- gui/mainwindow.h
+++ gui/mainwindow.h
@@ -6,7 +6,6 @@
 #include <QGraphicsScene>
 #include <QTimer>
 #include <QGraphicsItem>
-
 #include "delegate.h"
 
 #include "../main/supervisor/supervisor.hpp"
@@ -119,7 +118,6 @@ private:
     void colorize_ROI();
 
     void update_fps(int value);
-
 };
 
 
